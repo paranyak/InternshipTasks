926cfee41bbe692b023cc6aa26304ac72fbe2130
Natasha <paranyak@ucu.edu.ua>
2018-11-29 17:37:44 +0200

    Added fetching data.

--- src/api/fetch.js
+++ src/api/fetch.js
@@ -5,6 +5,18 @@ export const fetchUser = (name, password) => async (dispatch) => {
 
     dispatch(fromFetch.fetchUserStart(name, password));
     //let response = await fetch(`https://jsonplaceholder.typicode.com/users/${id}`);
+    let response_example = await fetch('http://127.0.0.1:3333/login', {
+        method: 'POST',
+        mode: 'cors',
+        headers:{
+            'Access-Control-Allow-Origin':'*',
+            "Content-type": "application/json"
+        },
+        body: JSON.stringify({"username": name, "password": password})
+    });
+    let result_example = await response_example.json();
+    console.log("TEMP: ",result_example);
+
     let response = {ok : true};
     if (!response.ok) {
         dispatch(fromFetch.fetchUserError(name, password));
